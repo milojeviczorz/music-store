60438d47bc95bf8f155ffd796b26e4eff8c08966
Zorz Milojevic <zorz.milojevic@rt-rk.com>
2017-09-26 21:53:45 +0200

    CORS mapping

--- src/main/java/com/music/store/app/App.java
+++ src/main/java/com/music/store/app/App.java
@@ -23,7 +23,7 @@ public class App {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("*");
+				registry.addMapping("/*").allowedOrigins("*");
 			}
 		};
 	}
